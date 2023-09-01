26a18f50cd907cf26afa7c95c23c0734ee6a73c1
DevDaniel1993 <Daniel072.dy@gmail.com>
2023-09-01 19:01:09 +0330

    Create Responsive Layout

--- Game-Hub/src/App.tsx
+++ Game-Hub/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        lg: `"nav nav" "aside main"`,
+        base: `"nav" "main"`,
+      }}
+      templateColumns={{
+        lg: "250px 1fr",
+        base: "1fr",
+      }}
+    >
+      <GridItem area={"nav"} bg="gold">
+        Nav
+      </GridItem>
+      <Show below="base">
+        <GridItem area={"aside"} bg="red">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
-
-export default App
+export default App;
