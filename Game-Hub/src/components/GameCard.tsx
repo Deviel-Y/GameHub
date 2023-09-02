16c4bf681d2fb70a8d640843d9a05e727f96a863
DevDaniel1993 <Daniel072.dy@gmail.com>
2023-09-02 15:52:20 +0330

    Get Optimized Images

--- Game-Hub/src/components/GameCard.tsx
+++ Game-Hub/src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import Metacritic from "./Metacritic";
+import getCropedImageUrl from "../services/image-url";
 
 interface Props {
   games: Game;
@@ -10,7 +11,7 @@ interface Props {
 function GameCard({ games }: Props) {
   return (
     <Card borderRadius="10px" overflow="hidden">
-      <Image src={games.background_image} />
+      <Image src={getCropedImageUrl(games.background_image)} />
       <Heading paddingX={5} paddingY={1} fontSize="2xl">
         {games.name}
       </Heading>
---
+++ Game-Hub/src/services/image-url.ts
@@ -0,0 +1,7 @@
+function getCropedImageUrl(url: string) {
+  const target: string = "media/";
+  const index: number = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+}
+
+export default getCropedImageUrl;
