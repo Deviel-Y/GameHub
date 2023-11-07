a10d3a19f5c280a46d716b3343a3b3d58176aabc
DevDaniel1993 <Daniel072.dy@gmail.com>
2023-11-07 21:05:33 +0330

    Fix the navbar

--- Game-Hub/src/components/NavBar.tsx
+++ Game-Hub/src/components/NavBar.tsx
@@ -2,11 +2,18 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ThemeSwitcher from "./ThemeSwicher";
 import SearchGames from "./SearchGames";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <HStack justifyContent="space-between" paddingY="5px">
-      <Image boxSize={{ lg: "65px", base: "75px" }} src={logo} />
+      <Link to="/">
+        <Image
+          boxSize={{ lg: "65px", base: "75px" }}
+          src={logo}
+          objectFit="cover"
+        />
+      </Link>
       <SearchGames />
       <ThemeSwitcher />
     </HStack>
--- Game-Hub/src/components/SearchGames.tsx
+++ Game-Hub/src/components/SearchGames.tsx
@@ -2,16 +2,20 @@ import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 function SearchGames() {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current?.value);
+        if (ref.current) {
+          setSearchText(ref.current?.value), navigate("/");
+        }
       }}
     >
       <InputGroup>
