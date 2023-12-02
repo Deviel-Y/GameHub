97ecedae383edd906b06dee169675e5e96e1ee90
Daniel Yadghar <daniel072.dy@gmail.com>
2023-12-02 11:59:58 +0330

    make GoToTheTop button appear smoothly

--- Game-Hub/src/components/GoUpButton.tsx
+++ Game-Hub/src/components/GoUpButton.tsx
@@ -1,20 +1,42 @@
 import { Link } from "@chakra-ui/react";
 import { BiArrowToTop } from "react-icons/bi";
+import { useState, useEffect } from "react";
 
 const GoUpButton = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 200) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Link
-      _hover={{
-        transform: "scale(1.2)",
-        transition: "all 0.2s ease-in",
-      }}
       href="#"
       variant={"solid"}
       backgroundColor={"green"}
       pos="fixed"
       borderRadius={10}
-      bottom="20px"
+      bottom={showButton ? "20px" : "-60px"} // Adjust the position based on showButton state
       right="20px"
+      opacity={showButton ? 1 : 0} // Show/hide the button based on showButton state
+      transition="bottom 0.5s, opacity 0.5s" // Smooth transition for bottom and opacity changes
+      _hover={{
+        transform: "scale(1.2)",
+        transition: "all 0.2s ease-in",
+      }}
     >
       <BiArrowToTop size="40px" />
     </Link>
